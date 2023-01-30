cba1c386531d7b1cd51d44148d56b0e3a7bf61b5
KorPavel <kor-p@bk.ru>
2023-01-30 16:31:18 +0300

    Update auth_page.py
    Файл с методами для страницы авторизации

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,13 +1,43 @@
 from .base_page import BasePage
 from .locators import AuthPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from ..settings import *
-from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait
 from datetime import datetime
+from time import sleep
+from settings import valid_user_data, get_nums, letters_en
 
 
 class AuthPage(BasePage):
+
+    def click_tab_phone(self):
+        """ Метод кликает по табу 'Телефон' и проверяет изменение его цвета """
+        phone_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_PHONE))
+        phone_tab.click()
+        assert 'active' in phone_tab.get_attribute('class')
+
+    def click_tab_email(self):
+        """ Метод кликает по табу 'Почта' и проверяет изменение его цвета """
+        email_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_MAIL))
+        email_tab.click()
+        assert 'active' in email_tab.get_attribute('class')
+
+    def click_tab_login(self):
+        """ Метод кликает по табу 'Логин' и проверяет изменение его цвета """
+        login_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_LOGIN))
+        login_tab.click()
+        assert 'active' in login_tab.get_attribute('class')
+
+    def click_tab_ls(self):
+        """ Метод кликает по табу 'Лицевой счёт' и проверяет изменение его цвета """
+        ls_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_LS))
+        ls_tab.click()
+        assert 'active' in ls_tab.get_attribute('class')
+
+    def insert_user_data_to_fields(self, login, password):
+        self.browser.find_element(*AuthPageLocators.LOGIN_AREA).send_keys(login)
+        self.browser.find_element(*AuthPageLocators.PASSWORD_EYE).click()  # Открыть пароль
+        self.browser.find_element(*AuthPageLocators.PASSWORD_AREA).send_keys(password)
+        self.browser.find_element(*AuthPageLocators.SUBMIT_BUTTON).click()
 
     def should_be_logo_on_header(self):
         """ Проверка наличия логотипа в хедере страницы """
@@ -61,49 +91,6 @@ class AuthPage(BasePage):
         sleep(2)
         assert account[1] in self.browser.current_url
 
-    def click_tab_phone(self):
-        """ Метод кликает по табу 'Телефон' и проверяет изменение его цвета """
-        phone_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_PHONE))
-        phone_tab.click()
-        tab_btn = phone_tab.get_attribute('class')
-        assert 'active' in tab_btn
-
-    def click_tab_email(self):
-        """ Метод кликает по табу 'Почта' и проверяет изменение его цвета """
-        email_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_MAIL))
-        email_tab.click()
-        tab_btn = email_tab.get_attribute('class')
-        assert 'active' in tab_btn
-
-    def click_tab_login(self):
-        """ Метод кликает по табу 'Логин' и проверяет изменение его цвета """
-        login_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_LOGIN))
-        login_tab.click()
-        tab_btn = login_tab.get_attribute('class')
-        assert 'active' in tab_btn
-
-    def click_tab_ls(self):
-        """ Метод кликает по табу 'Лицевой счёт' и проверяет изменение его цвета """
-        ls_tab = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_LS))
-        ls_tab.click()
-        tab_btn = ls_tab.get_attribute('class')
-        assert 'active' in tab_btn
-
-    def send_params_to_relevant_areas(self, login, pwd):
-        """ Метод заполняет поля логина и пароля соответствующими данными и отправляет их на проверку """
-        login_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.NAME_AREA))
-        login_area.send_keys(login)
-        pass_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.PASS_AREA))
-        pass_area.send_keys(pwd)
-        submit_button = self.wait.until(EC.presence_of_element_located(AuthPageLocators.LOGIN_BUTTON))
-        submit_button.click()
-
-    def insert_to_login_area(self, param):
-        login_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.NAME_AREA))
-        login_area.send_keys(param)
-        pass_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.PASS_AREA))
-        pass_area.click()
-
     def for_reset_bad_cases(self):
         """ Вспомогательный тест для проведения негативных тестов, чтобы не вводить
         цифро-буквенный код с картинки после ряда случаев неудачной авторизации пользователя """
@@ -114,6 +101,15 @@ class AuthPage(BasePage):
         logout_button.click()
         assert self.wait.until(EC.text_to_be_present_in_element(
             AuthPageLocators.PAGE_TITLE, 'Авторизация'))
+
+    def send_params_to_relevant_areas(self, login, pwd):
+        """ Метод заполняет поля логина и пароля соответствующими данными и отправляет их на проверку """
+        login_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.LOGIN_AREA))
+        login_area.send_keys(login)
+        pass_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.PASSWORD_AREA))
+        pass_area.send_keys(pwd)
+        submit_button = self.wait.until(EC.presence_of_element_located(AuthPageLocators.LOGIN_BUTTON))
+        submit_button.click()
 
     def check_auth_param_plus_pass(self, tab, param, password, exp=True):
         """ Проверка вариантов авторизации. При позитивном сценарии метод проверяет
@@ -158,6 +154,12 @@ class AuthPage(BasePage):
         tab_type = self.wait.until(EC.presence_of_element_located(AuthPageLocators.TAB_TYPE))
         assert tab.upper() == tab_type.get_attribute('value'), 'Таб не соответствует типу введённого параметра'
         print(tab_type.get_attribute('value'))
+
+    def insert_to_login_area(self, param):
+        login_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.LOGIN_AREA))
+        login_area.send_keys(param)
+        pass_area = self.wait.until(EC.presence_of_element_located(AuthPageLocators.PASSWORD_AREA))
+        pass_area.click()
 
     def check_phone_correct(self, tab, text, exp=True):
         ''' Проверка корректности номера телефона '''
@@ -205,3 +207,5 @@ class AuthPage(BasePage):
                 AuthPageLocators.MESSAGE_ERR, 'Неверный логин или пароль'))
         else:
             self.is_not_element_present(*AuthPageLocators.MESSAGE_ERR)
+
+
